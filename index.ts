db6ba00cd9ad9dc02d4707b4451734f9602bac36
Sam Wilkins <samwilkins333@gmail.com>
2019-09-27 04:02:23 -0400

    fixed module structure

--- index.ts
+++
@@ -1,8 +0,0 @@
-import BatchedArray from "./lib/BatchedArray/BatchedArray";
-import BatcherAgent from "./lib/BatcherAgent/BatcherAgent";
-export { BatchedArray, BatcherAgent }
-
-export { Interval, TimeUnit } from "./lib/BatchedArray/Interval";
-export { ThresholdBatcherSync, ThresholdBatcherAsync } from "./lib/BatcherAgent/BatcherAgents";
-
-
